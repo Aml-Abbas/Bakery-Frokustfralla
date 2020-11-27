0e8a1896f96157b0785721c44dcc0ad1da644763
Aml-Abbas <aml.abbas@hotmail.com>
2020-11-27 22:40:50 +0100

    fixed bakery-order-details

--- src/app/component/bakery-order-details/bakery-order-details.component.ts
+++ src/app/component/bakery-order-details/bakery-order-details.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import {Location, LocationStrategy, PathLocationStrategy} from '@angular/common';
+import {BakeryWeekOrdersItemDetails} from '../../model/BakeryWeekOrderItemDetalis';
+import {OrderDetailsService} from '../../../services/order-details.service';
 
 @Component({
   selector: 'app-bakery-order-details',
   templateUrl: './bakery-order-details.component.html',
-  styleUrls: ['./bakery-order-details.component.scss']
+  styleUrls: ['./bakery-order-details.component.scss'],
+  providers: [Location, {provide: LocationStrategy, useClass: PathLocationStrategy}]
+
 })
 export class BakeryOrderDetailsComponent implements OnInit {
+  location: Location;
+  title = 'Order details';
+  orderId = '';
+  itemDetails: BakeryWeekOrdersItemDetails | undefined;
 
-  constructor() { }
 
-  ngOnInit(): void {
+  constructor(location: Location,
+              private orderDetailsService: OrderDetailsService) {
+    this.location = location;
+
   }
 
+  ngOnInit(): void {
+    this.orderDetailsService.currentOrderId$.subscribe(orderId => this.orderId = orderId);
+    console.log('order id = ' + this.orderId);
+    this.title = 'Beställning #' + this.orderId;
+    this.itemDetails = this.orderDetailsService.fetchOrdersItemDetails();
+  }
+
+
+  navigateBack(): void {
+    this.location.back();
+  }
 }
